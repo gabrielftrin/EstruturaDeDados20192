bcc55ec4a9cb8484fc4591d88ee7fe109b35e2a2
gabrielftrin <mrgabrielfernandes@hotmail.com>
2019-10-17 03:00:13 -0300

    new

--- excecao.h
+++ excecao.h
@@ -1,69 +1,45 @@
-//erro erroDeExecucao;
-
-	/*printf("\nDigite o conteudo a ser validado: ");
-	fflush(stdin);
-	fgets(op, 14, stdin);*/
-
-	//if (strlen(op) = < 10) {	}
-	//else {	//erro.numeroDeCaracteresExcedido}
-
-
-
-///tudo é char
-// nenhum caractere pode ser virgula
-// nenhum caractece pode ser ponto
-// todo caractere deve ser digito 2 147 483 647
-
-//Ler 10 caracteres
-//se tiver 10
-	//o primeiro deve ser 1 ou 2
-//ver um caracter
-	//se o caracter é um dígito
-		//se não for retorne falso
-	//vá pro próximo caracter
 #include "estruturas.h"
-
-//#define numeroDeCaracteresExcedido "Numero de caracteres excedido."
-//#define erroInicio "Erro no inicio do programa."
-
-#define quantidadeDeErros 20
+#define quantidadeDeErros 10
 
 int listaDeCodigosDeErro[quantidadeDeErros];
+string listaDeMensagensDeErro[quantidadeDeErros];
 
 void lancarErro(int codigoDeErro) {
+
 	int i = 0;
-	if (listaDeCodigosDeErro[i] == 0)
+	while (listaDeCodigosDeErro[i] != 0)
 	{
-		listaDeCodigosDeErro[i] = codigoDeErro;
+		i++;
 	}
-	else {
-		while (listaDeCodigosDeErro[i] != 0)
-		{
-			if (listaDeCodigosDeErro[i] == 0)
-			{
-				listaDeCodigosDeErro[i] = codigoDeErro;
-				break;
-			}
-			i++;
-		}
-	}
+	listaDeCodigosDeErro[i] = codigoDeErro;	
 }
 
-typedef struct erro {
-	int codigo;
-	string mensagem;
-}erro;
+void imprimirListaDeErros() {
 
-erro meuerro;
+	int i = 0;
 
-meuerro.codigo = 5;
+	//Preencher o vetor
+	while (listaDeCodigosDeErro[i] != 0)
+	{
+		switch (listaDeCodigosDeErro[i])
+		{
+		case 1:
+			listaDeMensagensDeErro[i] = "Numero de caracteres excedido.";
+			break;
+		case 2:
+			listaDeMensagensDeErro[i] = "Erro no inicio do programa.";
+			break;
+		default:
+			break;
+		}
+		i++;
+	}
 
-/*
-erro numeroDeCaracteresExcedido;
-numeroDeCaracteresExcedido.codigo = 1;
-numeroDeCaracteresExcedido.string = "Numero de caracteres excedido.";
-
-erro erroInicio;
-erroInicio.codigo = 2;
-erroInicio.string = "Erro no inicio do programa..";
-*/
+	//Imprimir lista de erros
+	i = 0;
+	while (listaDeCodigosDeErro[i] != 0)
+	{
+		printf("\nMensagem de erro: %s\n", listaDeMensagensDeErro[i]);
+		i++;
+	}
+}
--- main.c
+++ main.c
@@ -4,35 +4,52 @@
 #include <string.h>
 #include "excecao.h"
 
-bool teste(int x) {
-	if (x > 0)
+void main()
+{
+	char n = "72";
+	printf("\nnumero completo: %s\n", n);
+	
+	char c = strpbrk(n,n);
+
+	printf("\nprim carc: %c\n", c);
+
+
+}
+/*
+//Valida se o número é natural até 2 147 483 647
+bool validarNumeroNatural(int numero) {
+
+	if (strlen(numero) > 10)
 	{
 		lancarErro(1);
 		return false;
 	}
-}
 
-bool funcao() {
-	int x = 0;
-	if (!teste(x)) {
-		lancarErro(2);
-		return false;
+	if (strlen(numero) == 10) {
+		if (true)
+		{
+
+		}
 	}
 }
-/*
-void retornarListaDeErros() {
-	imprimirListaDeErros();
-	destruirListaDeErros();
-}
+
+int op;
+printf("\nDigite o conteudo a ser validado: ");
+fflush(stdin);
+fgets(op, 14, stdin);
+
+if (strlen(op) = < 10) {}
+else { erro.numeroDeCaracteresExcedido }
 */
-void main()
-{
-	/*if (!funcao()){
-		retornarListaDeErros();
-		return	1;
-	}*/
+///tudo é char
+// nenhum caractere pode ser virgula
+// nenhum caractece pode ser ponto
+// todo caractere deve ser digito 2 147 483 647
 
-	printf("\n\ndeu tudo certo\n\n");
-
-	printf("\nres %d\n", meuerro.codigo);
-}
+//Ler 10 caracteres
+//se tiver 10
+	//o primeiro deve ser 1 ou 2
+//ver um caracter
+	//se o caracter é um dígito
+		//se não for retorne falso
+	//vá pro próximo caracter
