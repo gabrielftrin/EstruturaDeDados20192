a90c6e73634489613b12c82b80f1885943903411
Gabriel Fernandes Trindade <mrgabrielfernandes@hotmail.com>
2019-11-14 19:43:23 -0300

    Consultar uma palavra na árvore

--- main.c
+++ main.c
@@ -11,3 +11,26 @@ void main()
 		main();
 	}
 }
+/*
+
+bool consultarPalavraNaArvore() {
+
+	//Declara palavra a ser lida
+	string palavra = NULL;
+
+	//Pedir ao  usuário que digite a palavra
+	printf("\nDigite a palavra que deseja buscar: (Até 20 caracteres)\n\n");
+
+	//Ler palavra do teclado
+	palavra = lerTextoDoTeclado(20);
+
+	//Validar leitura
+	if (!validarAlocacaoDeMemoria(palavra)) return false;
+
+	//Verificar se a palavra está na árvore
+	if (!consultarUmaDeterminadaPalavraNaArvore(palavra)) return false;
+
+	return true;
+}
+
+*/
